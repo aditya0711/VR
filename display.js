dc54bda878c57edd8b36d8e239de390f52832c1d
aditya0711 <aditya071193@gmail.com>
2017-05-16 18:46:00 +0530

    initial commit

---
+++ display.js
@@ -0,0 +1,8 @@
+/**
+ * Created by adityaaggarwal on 15/5/17.
+ */
+var viewer = new FORGE.Viewer("container", "config.json");
+var viewer1 = new FORGE.Viewer("container2", "config1.json");
+var viewer2= new FORGE.Viewer("container3", "config2.json");
+var viewer3 = new FORGE.Viewer("container4", "config3.json");
+
